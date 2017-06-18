4700f1cc0a31be6a8a0b68e7d321cb2467bb506d
motosharpley <motosharpley@gmail.com>
2017-06-18 11:56:51 -0500

    add Articles functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+	
+	def edit
+		@article = Article.find(params[:id])	
+	end
+	
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Article was successfully created."
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:success] = "Your article was successfully updated"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
+	def show
+		@article = Article.find(params[:id])
+	end
+	
+	private
+		
+		def article_params
+			params.require(:article).permit(:title, :description)
+		end
+	
+end
